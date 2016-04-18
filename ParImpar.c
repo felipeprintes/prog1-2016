f2528456b2bfa9ed436ce85ea42e2d4c5a5c7fe8
Anderson Felipe Gomes Printes <felipe.unilasalle@gmail.com>
2016-04-18 13:09:31 +0000

    esse é um numero par?

---
+++ ParImpar.c
@@ -0,0 +1,20 @@
+//numero par ou impar
+#include <stdio.h>
+    int main()
+    {
+        int num;
+        printf("digite o numero aqui: ");  
+        scanf("%d", &num);
+            if(num%2==0){
+                
+            
+            printf("esse numero e par");
+            }
+            else
+            {
+                printf("esse numero e impar");
+            }
+            
+                
+            
+    }
---
+++ alunos.c
@@ -0,0 +1,9 @@
+\\ notas dos alunos
+#include <stdio>
+    int main( )
+    {
+        int aluno(a), notas, media;
+            printf("quem e o aluno: ");
+                scanf("%c", aluno);
+            
+    }
