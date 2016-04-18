97d94ab872b9c55a6e6b0ac0d823df0daa0c1c7e
Anderson Felipe Gomes Printes <felipe.unilasalle@gmail.com>
2016-04-18 12:07:44 +0000

    programa para calcular a media

---
+++ media.c
@@ -0,0 +1,20 @@
+// calcule a media aritmetica//
+#include <stdio.h>
+    int main()
+        {
+            int num1,num2,soma,media;
+           
+            printf("digite o primeiro numerio: ");
+            scanf("%d", &num1);
+            printf("digite o segundo numero: ");
+            scanf("%d", &num2);
+            soma=num1+num2;
+            media=(num1+num2)/2;
+            printf("media=%d", media);
+            
+            
+            
+            
+            
+            
+        }
